26f98803c594034967ca56eb98553f8b7ac598e2
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-05-16 21:29:54 +0200

    Add feedback item delete

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>;
-  console.log(feedback);
   return (
     <div className="feedback-list">
       {feedback.map((item, index) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
